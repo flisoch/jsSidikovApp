243bb072242204394b9881a718a92bff5e05126d
flisoch <flisochcs@gmail.com>
2018-09-02 23:37:31 +0300

    common part in each page

--- src/server.js
+++ src/server.js
@@ -1,19 +1,20 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-
+var hbs = require("hbs");
 var fs = require("fs");
 
 var app = express();
 
+hbs.registerPartials("/home/flisoch/IdeaProjects/jsWebApp/views/partials");
 app.set("view engine", "hbs");
-app.use(express.static('public'));
+
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 
 app.get("/reg", function(request, response){
 
-    response.sendFile('/home/flisoch/IdeaProjects/jsWebApp/public/registration.html');
+    response.render('/home/flisoch/IdeaProjects/jsWebApp/views/registration.hbs');
 });
 
 app.post("/reg",function (request,response) {
@@ -22,25 +23,30 @@ app.post("/reg",function (request,response) {
     var pass = request.body.user.password;
     fs.appendFile("users.txt", userName + " " + pass + "\n");
 
-    response.sendFile('/home/flisoch/IdeaProjects/jsWebApp/public/game.html');
+    response.redirect('/game');
 })
 
+app.get("/users",function (request,response) {
+    response.sendFile('/home/flisoch/IdeaProjects/jsWebApp/src/users.txt');
 
-app.get("/test",function (request,response) {
-    response.sendFile('/home/flisoch/IdeaProjects/jsWebApp/public/game.html');
+})
+
+app.get("/game",function (request,response) {
+    response.render('/home/flisoch/IdeaProjects/jsWebApp/views/game.hbs');
+})
+
+app.post("/records",function (request,response) {
+    //Todo:сохранять записи нормально
+    fs.appendFile("records.txt", request.body);
+    console.log(request.body)
+})
+app.get("/records",function (request,response) {
+    response.sendFile('/home/flisoch/IdeaProjects/jsWebApp/src/records.txt');
+
 })
 
 app.get("/",function (request,response) {
-    response.sendFile("/home/flisoch/IdeaProjects/jsWebApp/public/index.html")
-})
-app.post("/records",function (request,response) {
-    fs.appendFile("records.txt", request.body.toString());
-    console.log(request.body)
-})
-
-app.get("/records",function (request,response) {
-    var text = fs.readFile("records.txt");
-    console.log()
+    response.render("/home/flisoch/IdeaProjects/jsWebApp/views/index.hbs")
 })
 
 app.listen(3000);
